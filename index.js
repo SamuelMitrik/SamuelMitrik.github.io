c9c93f72416222c428b155b4dc3073890c54e552
SamuelMitrik <134398669+SamuelMitrik@users.noreply.github.com>
2023-05-31 18:52:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let username;
+document.getElementById("mybutton").onclick = function(){
+    username = document.getElementById("mytext").value;
+    console.log(username)
+    document.getElementById("mylabel").innerHTML = username
+}
+
+
